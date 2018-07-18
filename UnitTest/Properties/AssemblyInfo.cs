d98a69dfe53799cf81a92e054f77adc7006f9f0d
mamori017 <mamori017@outlook.jp>
2018-07-18 17:20:27 +0900

    Add test project

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b17eda8-0954-431f-b4e2-827fc365cdab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/TemplateTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeeklyBlogTemplate.Test
+{
+    [TestClass]
+    public class TemplateTest
+    {
+        readonly DateTime dt = DateTime.Parse("2018/07/18");
+
+        [TestMethod]
+        public void GetTitleFormatTest()
+        {
+            string answer = "2018年 第29週";
+            string output = Template.GetTitleFormat(DateEdit.GetWeekStartDate(dt), DateEdit.GetWeekCount(dt));
+
+            Assert.AreEqual(answer, output);
+        }
+
+        [TestMethod]
+        public void GetContentFormatTest()
+        {
+            string answer = "## 7/15=====##### ## 7/16=====##### ## 7/17=====##### ## 7/18=====##### ## 7/19=====##### ## 7/20=====##### ## 7/21=====##### ";
+            string output = Template.GetContentFormat(DateEdit.GetWeekStartDate(dt)).Replace("\n", "");
+
+            Assert.AreEqual(answer, output);
+        }
+
+        [TestMethod]
+        public void CreateOutputStringTest()
+        {
+            string answer = "[Title]2018年 第29週[Content]## 7/15=====##### ## 7/16=====##### ## 7/17=====##### ## 7/18=====##### ## 7/19=====##### ## 7/20=====##### ## 7/21=====##### ";
+            string output = Template.CreateOutputString(DateEdit.GetWeekStartDate(dt), DateEdit.GetWeekCount(dt)).Replace("\n","");
+
+            Assert.AreEqual(answer, output);
+        }
+    }
+}

b2aa96ea36167782ee5f68271e85f376ac70cfa5
mamori017 <mamori017@outlook.jp>
2018-07-24 17:30:39 +0900

    Add Message class test

---
+++ WeeklyBlogTemplateTests/MessageTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Common;
+using System.Reflection;
+
+namespace WeeklyBlogTemplate.Tests
+{
+    [TestClass()]
+    public class MessageTests
+    {
+        [TestMethod()]
+        public void ShowTitleTest()
+        {
+            Message.ShowTitle();
+
+            Assert.AreEqual(true,true);
+        }
+    }
+}
